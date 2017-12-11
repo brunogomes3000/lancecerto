c3187f9cae5e1c5ea6f411629aa8d233f5ed13a0
Wolverinealbuquerque <gabriels.felipes@gmail.com>
2017-12-11 17:26:38 -0300

    Atualizei

--- lancecerto/core/views.py
+++ lancecerto/core/views.py
@@ -16,14 +16,10 @@ def relatoriosvendas(request):
 def cadastro(request):
     return render(request, 'cadastro.html')
 
-<<<<<<< HEAD
 def produtos(request):
     return render(request, 'produtos.html')
 
 def dados_prod(request):
     return render(request, 'dados_prod.html')
-
-=======
->>>>>>> 1107f6fb4eafe494cbcdc27394c375f621765bb2
 
 # Create your views here.
