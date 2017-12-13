1d1cfbdcfd3a7d4658a49f336fd9b8d2d2fe8287
JeffersonCypriano <jeffersoncypri@gmail.com>
2017-12-13 15:16:01 -0300

    agr está listando produtos e criei pagina c detalhes dos produtos

--- lancecerto/core/views.py
+++ lancecerto/core/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Produtos
 
 def index(request):
     return render(request, 'index.html')
@@ -24,3 +25,18 @@ def produtos(request):
 def dados_prod(request):
     return render(request, 'dados_prod.html')
 # Create your views here.
+
+def produtos(request):
+	produtos = Produtos.objects.all()
+	context = {
+		'produtos': produtos
+	}
+	return render(request, 'produtos.html', context)
+
+def produtos_detalhes(request):
+	id_produto = request.GET.get("id")
+	produto = Produtos.objects.get(id=id_produto)
+	context = {
+		'produto': produto
+	}
+	return render(request, 'produtos_detalhes.html', context)
